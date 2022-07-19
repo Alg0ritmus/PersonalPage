6e3d06761c5fb8b0d5611d4ea332f73277d4a546
unknown <P.zelenak1@gmail.com>
2022-07-19 17:18:37 +0200

    asdas

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const app = express();
 const Routerik = require('./router');
 
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 //load static files scss,imgs...
 app.use(express.json());
 app.use(express.static('./frontend/static')); 
